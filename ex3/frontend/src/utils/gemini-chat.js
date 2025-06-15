b871cbc5e7e46ebe9c7d9e31e4da2f3eddff9867
S3annnyyy <seanyckang@gmail.com>
2025-06-15 20:08:02 +0800

    Initil commit

--- ex3/frontend/src/utils/gemini-chat.js
+++ ex3/frontend/src/utils/gemini-chat.js
@@ -5,9 +5,20 @@ export const geminiChatCompletion = async (message) => {
     // Exercise 3 part 2
     // make a POST request to your backend’s /api/v1/gemini-chat endpoint using axios.post
     // Send the user’s message as the request body under userPrompt.
-    // Set the Content-Type header to application/json.
+    // Set the Content-Type header to application/json
     // TODO
-    
+    // const response = await axios.post(
+    //   `${import.meta.env.VITE_API_URL}/api/v1/gemini-chat`,
+    //   {
+    //     userPrompt: message
+    //   },
+    //   {
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     }
+    //   }
+    // );
+    // console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching issues', error);
